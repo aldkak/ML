bcb7cc648ea5ddcba2909a0fd346f1fc73fc4aec
MSaif <m_aldkak_s@yahoo.com>
2018-03-15 19:34:19 +0500

    Update dogs_vs_cats.py

--- dogs vs cats/dogs_vs_cats.py
+++ dogs vs cats/dogs_vs_cats.py
@@ -1,51 +1,50 @@
-# Распознавание собак и кошек на изображениях с помощью сверточной нейронной сети
-# Источник данных - соревнования Kaggle Dogs vs. Cats.
-# Для распознавания используется сверточная нейронная сеть.
-# Перед использованием необходимо скачать и подготовить данные для обучения, проверки и   
-# тестирования. Можно использовать пример в ноутбуке data_preparation
+# Recognition of dogs and cats on images using a convolutional neural network CNN
+# Data source - Kaggle Dogs vs. competition. Cats.
+# Before using, you must download and prepare data for training, testing
 
-# step 1
+# step 1  import models 
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
-# step2
-# Каталог с данными для обучения
+# step2 
+# folder of trained data
 train_dir = 'train'
-# Каталог с данными для проверки
+# data for checking 
 val_dir = 'val'
-# Каталог с данными для тестирования
+# test folder
 test_dir = 'test'
-# Размеры изображения
+# size of the image
 img_width, img_height = 150, 150
-# Размерность тензора на основе изображения для входных данных в нейронную сеть
+# The dimension of the tensor on the basis of the image for the input data to the neural network
 # backend Tensorflow, channels_last
 input_shape = (img_width, img_height, 3)
-# Количество эпох
+# Number of epochs
 epochs = 30
-# Размер мини-выборки
+# Size of the mini-sample
 batch_size = 16
-# Количество изображений для обучения
+# Number of images for training
 nb_train_samples = 17500
-# Количество изображений для проверки
+# Number of images to check
 nb_validation_samples = 3750
-# Количество изображений для тестирования
+# Number of images for testing
 nb_test_samples = 3750
-#Создаем сверточную нейронную сеть
-#Архитектура сети
 
-#Слой свертки, размер ядра 3х3, количество карт признаков - 32 шт., функция активации ReLU.
-#Слой подвыборки, выбор максимального значения из квадрата 2х2
-#Слой свертки, размер ядра 3х3, количество карт признаков - 32 шт., функция активации ReLU.
-#Слой подвыборки, выбор максимального значения из квадрата 2х2
-#Слой свертки, размер ядра 3х3, количество карт признаков - 64 шт., функция активации ReLU.
-#Слой подвыборки, выбор максимального значения из квадрата 2х2
-#Слой преобразования из двумерного в одномерное представление
-#Полносвязный слой, 64 нейрона, функция активации ReLU.
-#Слой Dropout.
-#Выходной слой, 1 нейрон, функция активации sigmoid
-#Слои с 1 по 6 используются для выделения важных признаков в изображении, а слои с 7 по 10 -
-#для классификации.
+
+# Create convolutional neural network
+# Network Architecture
+# Convolution layer, kernel size 3x3, number of feature cards - 32 pieces, activation function ReLU.
+# Sub-sample layer, selection of the maximum value from the 2x2 square
+# Convolution layer, kernel size 3x3, number of feature cards - 32 pieces, activation function ReLU.
+# Sub-sample layer, selection of the maximum value from the 2x2 square
+# Convolution layer, kernel size 3x3, number of feature cards - 64 pcs., Activation function ReLU.
+# Sub-sample layer, selection of the maximum value from the 2x2 square
+# Conversion layer from two-dimensional to one-dimensional representation
+# Fully-knit layer, 64 neurons, activation function ReLU.
+# Dropout layer.
+# Output layer, 1 neuron, activation function sigmoid
+# Layers 1 through 6 are used to highlight important characteristics in the image, and layers 7 to 10 -
+# for classification.
 
 #step 3
 model = Sequential()
@@ -67,19 +66,19 @@ model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
-#Компилируем нейронную сеть
+# Compile the neural network
 
 # step4
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-#Создаем генератор изображений
-#Генератор изображений создается на основе класса ImageDataGenerator. Генератор делит значения 
-#всех пикселов изображения на 255.
+# Create an image generator
+# The image generator is created based on the ImageDataGenerator class. The generator divides the values
+# all pixels of the image at 255.
 
 #step5
 datagen = ImageDataGenerator(rescale=1. / 255)
-#Генератор данных для обучения на основе изображений из каталога
+# Data generator for learning based on images from the folder
 
 #step6
 train_generator = datagen.flow_from_directory(
@@ -87,28 +86,26 @@ train_generator = datagen.flow_from_directory(
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
-#Found 17500 images belonging to 2 classes.
-#Генератор данных для проверки на основе изображений из каталога
 
+# Data generator for checking based on images from the folder
 #step7
 val_generator = datagen.flow_from_directory(
     val_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
-#Found 3750 images belonging to 2 classes.
-#Генератор данных для тестирования на основе изображений из каталога
+# Data generator for testing based on images from the folder
 #step8
 test_generator = datagen.flow_from_directory(
     test_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
-#Found 3750 images belonging to 2 classes.
-#Обучаем модель с использованием генераторов
-#train_generator - генератор данных для обучения
 
-#validation_data - генератор данных для проверки
+# Train a model using generators
+#train_generator - data generator for learning
+
+#validation_data 
 
 #step9
 model.fit_generator(
@@ -118,7 +115,6 @@ model.fit_generator(
     validation_data=val_generator,
     validation_steps=nb_validation_samples // batch_size)
 
-#step31
 scores = model.evaluate_generator(test_generator, nb_test_samples // batch_size)
-#step32
-print("Аккуратность на тестовых данных: %.2f%%" % (scores[1]*100))
+
+print("Accuracy on the test data: %.2f%%" % (scores[1]*100))
