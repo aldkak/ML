32c84ab13c00ff9bcb96977a9d4dcfd3ebdb0c66
MSaif <m_aldkak_s@yahoo.com>
2018-04-04 17:34:07 +0500

    Create cony_edge.py

---
+++ start/cony_edge.py
@@ -0,0 +1,22 @@
+import cv2
+import argparse
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+#-------------------------------------------------------
+
+img = cv2.imread(args["image"],0)
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
